fc12a48ee8fba50ee074df82947f30188aa27042
mwz73trz <michaelzura@bellsouth.net>
2022-02-08 11:02:09 -0500

    fixed pax

--- frontend/src/routes/passengersList.js
+++ frontend/src/routes/passengersList.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet, useSearchParams } from "react-router-dom";
 import flightBookingAPI from "../api/flightBookingAPI";
-import Passengers from "../components/Passengers";
 
 export default function PassengersList() {
   const [passengers, setPassengers] = useState([]);
+  let [searchParams, setSearchParams] = useSearchParams();
 
   useEffect(() => {
     const getPassengers = async () => {
@@ -16,17 +16,57 @@ export default function PassengersList() {
 
     getPassengers();
   }, []);
-  return (
-    <div style={{ display: "flex" }}>
-      <nav
-        style={{
-          borderRight: "solid 1px",
-          padding: "1rem",
-        }}
-      >
-        <Passengers passengers={passengers} />
-      </nav>
-      <Outlet />
-    </div>
-  );
+
+  const renderPassengers = () => {
+    if (!passengers) {
+      return null;
+    }
+    return (
+      <div style={{ display: "flex" }}>
+        <nav
+          style={{
+            borderRight: "solid 1px",
+            padding: "1rem",
+          }}
+        >
+          <input
+            value={searchParams.get("filter") || ""}
+            onChange={(event) => {
+              let filter = event.target.value;
+              if (filter) {
+                setSearchParams({ filter });
+              } else {
+                setSearchParams({});
+              }
+            }}
+          />
+          {passengers
+            .filter((passenger) => {
+              let filter = searchParams.get("filter");
+              if (!filter) return true;
+              let last_name = passenger.last_name.toLowerCase();
+              return last_name.startsWith(filter.toLowerCase());
+            })
+            .map((passenger) => (
+              <NavLink
+                style={({ isActive }) => {
+                  return {
+                    display: "block",
+                    margin: "1rem 0",
+                    color: isActive ? "red" : "",
+                  };
+                }}
+                to={`/passengers/${passenger.id}`}
+                key={passenger.id}
+              >
+                {passenger.last_name}, {passenger.first_name}
+              </NavLink>
+            ))}
+        </nav>
+        <Outlet />
+      </div>
+    );
+  };
+
+  return <div>{renderPassengers()}</div>;
 }
